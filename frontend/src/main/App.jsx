417531b87270e337950c919a3920b7b9d5a0392f
unknown <dkamillythauanny@gmail.com>
2025-10-27 11:41:19 -0300

    atualizando projeto

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -26,15 +26,15 @@
 
 
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'boorstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min';
 import './App.css';
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from '../auth/AuthContent'; 
+import { AuthProvider } from '../auth/AuthContent';
 import PrivateRoute from '../auth/PrivateRoute';
 
-// Componentes principais
+//Componentes principais
 import Logo from '../components/template/Logo';
 import Nav from '../components/template/Nav';
 
@@ -50,8 +50,35 @@ function App(){
   return (
     <AuthProvider>
       <Router>
-        <div className="app"> {/* Layout principal com grid e sem fundo branco*/} 
+        <div className="app"> {/* layout principal com grid e sem fundo branco */}
+        {/*  Exibe a estrutura fixa apenas se o usuário estiver logado*/}
+          <PrivateRoute>
+            <Logo/ >
+            <Nav/ >
+          </PrivateRoute>   
+          <main className="app-content">
+            <Routes>
+              {/* Rotas públicas */}
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/register" element={<Register/>}/>
 
+              {/* Rotas privadas  */}
+              <Route 
+                path="/"
+
+                element={
+                  <PrivateRoute>
+                    <Home />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                
+              />
+
+            </Routes>
+          </main>
         </div>
       </Router>
     </AuthProvider>
