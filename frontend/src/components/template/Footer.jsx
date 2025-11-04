18f6eac10084ebecc8d357667a32a4e395424c7a
unknown <dkamillythauanny@gmail.com>
2025-11-04 09:38:45 -0300

    adicionando arquivos do template

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -1,3 +1,13 @@
 import './Footer.css';
 import React from 'react';
 
+const Footer = () => (
+    <footer className="footer">
+        <span>
+            Desenvolvido por Kamilly
+        </span>
+    </footer>
+);
+
+export default Footer;
+
